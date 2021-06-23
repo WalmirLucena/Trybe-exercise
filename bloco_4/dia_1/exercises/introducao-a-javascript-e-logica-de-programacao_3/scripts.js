e153f47fc7675edcd4a51ba0ae2882c27febcc94
WalmirLucena <walmirlucena3@gmail.com>
2021-06-23 17:31:37 -0400

    Adiciona Questao 3

---
+++ bloco_4/dia_1/exercises/introducao-a-javascript-e-logica-de-programacao_3/scripts.js
@@ -0,0 +1,15 @@
+let a =20;
+let b= 130;
+let c=74;
+
+if(a>b && a>c){
+    console.log(a);
+} else if(a>b & a<c){
+    console.log(c);
+} else if(b>a && b>c){
+    console.log(b);
+} else if (b>a && b<c){
+    console.log(c);
+} else if(c>a && c>b){
+    console.log(c);
+}
