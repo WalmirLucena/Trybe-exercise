8b49c6704a9c87c1780444f56ff4cee4de53fada
WalmirLucena <walmirlucena3@gmail.com>
2021-06-24 10:59:42 -0400

    Adiciona Exercicios e Aula 4.1

---
+++ bloco_4/dia_1/aula/scripts.js
@@ -0,0 +1,14 @@
+
+let nome = "walmir";
+ switch(nome){
+     case "Atanes":
+         console.log("Atanes");
+         break;
+    case "walmir":
+        console.log("Walmir");
+        break;
+    case "Gus":
+        console.log("Gus");
+    default:
+        console.log('nenhum');
+ }
